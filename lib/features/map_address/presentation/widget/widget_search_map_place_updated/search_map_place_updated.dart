c16e9d7196c61b2b8800279ee1106dafaf1c8fe3
moatazmohamed <mohamed.moataz194@gmail.com>
2024-10-01 06:03:30 +0300

    textfield of get location

---
+++ lib/features/map_address/presentation/widget/widget_search_map_place_updated/search_map_place_updated.dart
@@ -0,0 +1,16 @@
+library search_map_place_updated;
+
+import 'package:flutter/material.dart';
+import 'dart:convert' as JSON;
+
+import 'package:http/http.dart' as http;
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+import '../../../../../core/app_color.dart';
+import '../../../../../core/border_manager.dart';
+
+part 'src/geocoding.dart';
+part 'src/geolocation.dart';
+part 'src/place.dart';
+part 'src/placeType.dart';
+part 'src/searchMapPlaceWidget.dart';
